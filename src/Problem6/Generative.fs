a7ff5e35fd2a8e40f12d15950a616ac1883ba2e2
pavel <pavel.alex.030406@gmail.com>
2025-09-30 20:04:27 +0300

    тест сокращения кода

--- src/Problem6/Generative.fs
+++ src/Problem6/Generative.fs
@@ -1,15 +1,12 @@
 module Problem6.Generative
 
-let sum acc x=
-    acc + x
-
 let eulerFunc n=
     //генерация последовательностей
     let numbers = [1..n] 
     let squares = [for i in 1..n -> i*i]
     //свертка
-    let numbersSum = numbers |> List.fold(sum) 0
-    let squareSum= squares |> List.fold(sum) 0
+    let numbersSum = numbers |> List.fold(+) 0
+    let squareSum= squares |> List.fold(+) 0
     numbersSum*numbersSum - squareSum
 
     
